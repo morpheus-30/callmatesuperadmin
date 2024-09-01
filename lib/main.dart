df2d2022318a0b1891ecaa1b2169d8c4e8795b40
morpheus-30 <konnichiwasekai7@gmail.com>
2024-09-01 13:16:33 +0530

    remove options

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:callmatesuperadmin/Screens/login_screen.dart';
-import 'package:callmatesuperadmin/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
